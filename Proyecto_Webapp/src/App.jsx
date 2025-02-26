e5f91d0f2df0563565757cb2ea4e8fef9de59af0
Paulafernandeztorres <paufeto20@gmail.com>
2025-02-26 16:00:26 +0100

    init comit

--- Proyecto_Webapp/src/App.jsx
+++ Proyecto_Webapp/src/App.jsx
@@ -1,16 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import Header from "./components/Header.jsx";
+import Footer from "./components/Footer.jsx";
 
 function App() {
- 
-
   return (
     <>
-      
+      <Header />
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ Proyecto_Webapp/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+import "../styles/Header.css";
+
+function Header() {
+  return (
+    <header className="header">
+    </header>
+  );
+}
+
+export default Header;
