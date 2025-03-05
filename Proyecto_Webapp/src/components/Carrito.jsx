f45af87edc0c4a098e8745c2e2d4bf994db0b096
Paulafernandeztorres <paufeto20@gmail.com>
2025-03-05 15:30:46 +0100

    Arreglo carrito

--- Proyecto_Webapp/src/components/Carrito.jsx
+++ Proyecto_Webapp/src/components/Carrito.jsx
@@ -19,15 +19,16 @@ function Carrito({ carrito, productosFirebase, agregarAlCarrito, eliminarDelCarr
             <ListGroup.Item key={producto.id} className="d-flex align-items-center">
               <Image src={producto.imagen} rounded style={{ width: '50px', height: '50px', objectFit: 'cover' }} />
               <span style={{ flex: 2, marginLeft: '15px' }}>{producto.nombre}</span>
-              <div className="d-flex align-items-center">
+              <div className="d-flex align-items-center justify-content-center" style={{ flex: 1 }}>
                 <Button variant="danger" onClick={() => eliminarDelCarrito(producto.id)}>-</Button>
-                <span style={{ margin: '0 10px' }}>{carrito[producto.id]}</span>
+                <span className="product-quantity" style={{ margin: '0 10px', textAlign: 'center' }}>{carrito[producto.id]}</span>
                 <Button variant="primary" onClick={() => agregarAlCarrito(producto.id)}>+</Button>
               </div>
               <Button 
                 variant="danger" 
                 onClick={() => eliminarProductoDelCarrito(producto.id)} 
-                style={{ flex: 1, padding: '6px 12px' }} 
+                className="delete-button"
+                style={{ marginLeft: 'auto' }}
               >
                 Eliminar
               </Button>
