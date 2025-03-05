69fd5f2d0566542aa683a723c56a5661e0855435
miguelable <miguelff222@gmail.com>
2025-03-05 15:56:38 +0100

    Añade el componente Login con formulario de inicio de sesión y estilos, y actualiza el enrutamiento en App.jsx. Modifica el pie de página para reflejar el nuevo año.

--- Proyecto_Webapp/src/components/Footer.jsx
+++ Proyecto_Webapp/src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <footer className="container mt-5 mt-auto">
       <p>
-        &copy; 2017–2025 Company, Inc. &middot;
+        &copy; 2024–2025 Marketplace, Inc. &middot;
         <a href="#">Privacidad</a> &middot;
         <a href="#">Términos</a>
       </p>
