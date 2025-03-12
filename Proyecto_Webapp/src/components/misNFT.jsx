f2e7a8d8729ebec059a61ff041fca1312aea7257
miguelable <miguelff222@gmail.com>
2025-03-12 18:39:58 +0100

    Añade lógica de navegación y mensajes de alerta en el componente MisNFT para usuarios no autenticados

--- Proyecto_Webapp/src/components/misNFT.jsx
+++ Proyecto_Webapp/src/components/misNFT.jsx
@@ -1,19 +1,60 @@
 import { useEffect, useState } from "react";
 import PropTypes from "prop-types";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Button } from "react-bootstrap";
 import { contarProductosComprados } from "../utils/firebase.utils";
 import ItemComprado from "./ItemComprado";
 import "../styles/MisNFT.css"; // Importar el archivo CSS
+import { useNavigate } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faExclamationTriangle } from "@fortawesome/free-solid-svg-icons";
 
 const MisNFT = ({ usuario, productosFirebase }) => {
   const [productosContados, setProductosContados] = useState({});
+  const navigate = useNavigate();
 
   useEffect(() => {
-    if (usuario.Comprados) {
+    if (usuario && usuario.Comprados) {
       const productos = contarProductosComprados(usuario.Comprados);
       setProductosContados(productos);
     }
   }, [usuario]);
+
+  const handleLogin = () => {
+    navigate("/login");
+  };
+
+  const handleViewProducts = () => {
+    navigate("/productos");
+  };
+
+  if (!usuario) {
+    return (
+      <Container className="d-flex justify-content-center align-items-center vh-100">
+        <div className="text-center p-4 border rounded shadow-sm bg-light">
+          <FontAwesomeIcon
+            icon={faExclamationTriangle}
+            size="3x"
+            className="mb-3 text-warning"
+          />
+          <h2>Debes iniciar sesión para ver tus NFT</h2>
+          <Button
+            onClick={handleLogin}
+            className="w-auto mt-3 inicio-button"
+            variant="primary"
+          >
+            Iniciar Sesión
+          </Button>
+          <Button
+            onClick={handleViewProducts}
+            className="w-auto mt-3 btn"
+            variant="secondary"
+          >
+            Ver Productos
+          </Button>
+        </div>
+      </Container>
+    );
+  }
 
   return (
     <Container className="productos-container">
