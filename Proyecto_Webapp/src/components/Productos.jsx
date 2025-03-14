8d0d881b73c4c33692f6b8350476bae85e719415
Paulafernandeztorres <paufeto20@gmail.com>
2025-03-14 12:48:40 +0100

    Añade funcionalidad de búsqueda y ordenamiento en el componente Productos; incluye un selector para ordenar por precio.

--- Proyecto_Webapp/src/components/Productos.jsx
+++ Proyecto_Webapp/src/components/Productos.jsx
@@ -11,25 +11,45 @@ function Productos({
   carrito,
 }) {
   const [searchTerm, setSearchTerm] = useState("");
+  const [sortOrder, setSortOrder] = useState("");
 
   const handleSearchChange = (event) => {
     setSearchTerm(event.target.value);
   };
 
-  const filteredProductos = productosFirebase.filter((producto) =>
-    producto.nombre.toLowerCase().includes(searchTerm.toLowerCase())
-  );
+  const handleSortChange = (event) => {
+    setSortOrder(event.target.value);
+  };
+
+  const filteredProductos = productosFirebase
+    .filter((producto) =>
+      producto.nombre.toLowerCase().includes(searchTerm.toLowerCase())
+    )
+    .sort((a, b) => {
+      if (sortOrder === "asc") {
+        return a.precio - b.precio;
+      } else if (sortOrder === "desc") {
+        return b.precio - a.precio;
+      } else {
+        return 0;
+      }
+    });
 
   return (
     <Container className="productos-container">
-      <Form.Group controlId="search" className="mb-4">
+      <Form.Group controlId="search" className="mb-4 d-flex">
         <Form.Control
           type="text"
           placeholder="Buscar productos por nombre"
           value={searchTerm}
           onChange={handleSearchChange}
-          className="search-input"
+          className="search-input me-2"
         />
+        <Form.Control as="select" value={sortOrder} onChange={handleSortChange} className="sort-select" style={{ width: "200px" }}>
+          <option value="">Ordenar por precio</option>
+          <option value="asc">Menor a Mayor</option>
+          <option value="desc">Mayor a Menor</option>
+        </Form.Control>
       </Form.Group>
       <h2>Productos disponibles:</h2>
       <Row>
@@ -51,6 +71,7 @@ Productos.propTypes = {
     PropTypes.shape({
       id: PropTypes.string.isRequired,
       nombre: PropTypes.string.isRequired,
+      precio: PropTypes.number.isRequired,
     })
   ).isRequired,
   agregarAlCarrito: PropTypes.func.isRequired,
