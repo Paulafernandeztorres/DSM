66b30b1cd804bb53809c2b8d4444da11f9bd0403
Paulafernandeztorres <paufeto20@gmail.com>
2025-03-27 12:53:43 +0100

    Componentes funcionales en React Native

--- appGaztaroaJavascript/App.js
+++ appGaztaroaJavascript/App.js
@@ -1,16 +1,15 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
 import { View } from "react-native";
+import React from "react";
 import Campobase from "./componentes/CampobaseComponent";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 export default function App() {
   return (
     <SafeAreaProvider>
-      {" "}
       <View>
-        {" "}
-        <Campobase /> <StatusBar style="auto" />{" "}
-      </View>{" "}
+        <Campobase />
+        <StatusBar style="auto" />
+      </View>
     </SafeAreaProvider>
   );
 }
--- appGaztaroaJavascript/componentes/CalendarioComponent.js
+++ appGaztaroaJavascript/componentes/CalendarioComponent.js
@@ -1,28 +1,33 @@
 import React from "react";
 import { ListItem, Avatar } from "@rneui/themed";
-import { SafeAreaView, FlatList } from "react-native";
+import { SafeAreaView, FlatList, Text } from "react-native";
 function Calendario(props) {
   const renderCalendarioItem = ({ item, index }) => {
     return (
-      <ListItem key={index} bottomDivider>
-        {" "}
-        <Avatar source={require("./imagenes/40Años.png")} />{" "}
+      <ListItem
+        key={index}
+        onPress={() => props.onPress(item.id)}
+        bottomDivider
+      >
+        <Avatar source={require("./imagenes/40Años.png")} />
         <ListItem.Content>
-          {" "}
-          <ListItem.Title>{item.nombre}</ListItem.Title>{" "}
-          <ListItem.Subtitle>{item.descripcion}</ListItem.Subtitle>{" "}
-        </ListItem.Content>{" "}
+          <ListItem.Title>
+            <Text>{item.nombre}</Text>
+          </ListItem.Title>
+          <ListItem.Subtitle>
+            <Text>{item.descripcion}</Text>
+          </ListItem.Subtitle>
+        </ListItem.Content>
       </ListItem>
     );
   };
   return (
     <SafeAreaView>
-      {" "}
       <FlatList
         data={props.excursiones}
         renderItem={renderCalendarioItem}
         keyExtractor={(item) => item.id.toString()}
-      />{" "}
+      />
     </SafeAreaView>
   );
 }
