0ec88865ef90d8f34a22273763977e4dd940d4e7
Paulafernandeztorres <paufeto20@gmail.com>
2025-03-31 13:25:14 +0200

    Stack Navigation

--- appGaztaroaJavascript/componentes/DetalleExcursionComponent.js
+++ appGaztaroaJavascript/componentes/DetalleExcursionComponent.js
@@ -1,14 +1,15 @@
-import React from "react";
+import React, { Component } from "react";
 import { Text, View } from "react-native";
 import { Card } from "@rneui/themed";
+import { EXCURSIONES } from "../comun/excursiones";
+
 function RenderExcursion(props) {
   const excursion = props.excursion;
+
   if (excursion != null) {
     return (
       <Card>
-        <Card.Title>
-          <Text>{excursion.nombre}</Text>
-        </Card.Title>
+        <Card.Title>{excursion.nombre}</Card.Title>
         <Card.Divider />
         <Card.Image source={require("./imagenes/40Años.png")}></Card.Image>
         <Text style={{ margin: 20 }}>{excursion.descripcion}</Text>
@@ -18,7 +19,19 @@ function RenderExcursion(props) {
     return <View></View>;
   }
 }
-function DetalleExcursion(props) {
-  return <RenderExcursion excursion={props.excursion} />;
+
+class DetalleExcursion extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      excursiones: EXCURSIONES,
+    };
+  }
+
+  render() {
+    const { excursionId } = this.props.route.params;
+    return <RenderExcursion excursion={this.state.excursiones[+excursionId]} />;
+  }
 }
+
 export default DetalleExcursion;
