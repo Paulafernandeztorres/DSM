68d531c5a2ffbe6165e6c397eda161afdfb5a758
Paulafernandeztorres <paufeto20@gmail.com>
2025-04-09 13:33:43 +0200

    Ejercicio Componentes y Navegacion 2

--- appGaztaroaJavascript/componentes/QuienesSomosComponent.js
+++ appGaztaroaJavascript/componentes/QuienesSomosComponent.js
@@ -1,10 +1,38 @@
 import React, { Component } from "react";
-import { ScrollView, View, Image } from "react-native";
+import { ScrollView, View, Image, FlatList } from "react-native";
 import { Card, Text } from "@rneui/themed";
 import Historia from "./HistoriaComponent";
 import { ACTIVIDADES } from "../comun/actividades";
 
 class QuienesSomos extends Component {
+  renderActividad = ({ item }) => (
+    <View style={{ marginBottom: 15 }}>
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          marginLeft: 20,
+        }}
+      >
+        <Image
+          source={require("./imagenes/40Años.png")}
+          style={{ width: 40, height: 40, marginRight: 15 }}
+        />
+        <View style={{ flex: 1 }}>
+          <Text style={{ fontSize: 16 }}>{item.nombre}</Text>
+          <Text>{item.descripcion}</Text>
+        </View>
+      </View>
+      <View
+        style={{
+          borderBottomWidth: 1,
+          borderBottomColor: "#ddd",
+          marginVertical: 10,
+        }}
+      />
+    </View>
+  );
+
   render() {
     return (
       <ScrollView>
@@ -14,35 +42,12 @@ class QuienesSomos extends Component {
           <Card>
             <Card.Title>Actividades y recursos</Card.Title>
             <Card.Divider />
-            {ACTIVIDADES.map((actividad, index) => (
-              <View key={index} style={{ marginBottom: 15 }}>
-                <View
-                  style={{
-                    flexDirection: "row",
-                    alignItems: "center",
-                    marginLeft: 20,
-                  }}
-                >
-                  <Image
-                    source={require("./imagenes/40Años.png")}
-                    style={{ width: 40, height: 40, marginRight: 15 }}
-                  />
-                  <View style={{ flex: 1 }}>
-                    <Text style={{ fontSize: 16 }}>{actividad.nombre}</Text>
-                    <Text>{actividad.descripcion}</Text>
-                  </View>
-                </View>
-                {index < ACTIVIDADES.length - 1 && (
-                  <View
-                    style={{
-                      borderBottomWidth: 1,
-                      borderBottomColor: "#ddd",
-                      marginVertical: 10,
-                    }}
-                  />
-                )}
-              </View>
-            ))}
+            <FlatList
+              data={ACTIVIDADES}
+              renderItem={this.renderActividad}
+              keyExtractor={(item, index) => index.toString()}
+              scrollEnabled={false}
+            />
           </Card>
         </View>
       </ScrollView>
