01e02a1dd796d37be4f7b4c13008fe63c976ae56
Paulafernandeztorres <paufeto20@gmail.com>
2025-05-01 13:11:14 +0200

    Ejercicio Formularios y Modals con Redux

--- appGaztaroaJavascript/componentes/DetalleExcursionComponent.js
+++ appGaztaroaJavascript/componentes/DetalleExcursionComponent.js
@@ -1,10 +1,20 @@
-import React, { Component } from "react";
-import { Text, View, StyleSheet, ScrollView, FlatList } from "react-native";
-import { Card, Icon } from "@rneui/themed";
+import React, { Component, useState } from "react";
+import {
+  Text,
+  View,
+  StyleSheet,
+  ScrollView,
+  FlatList,
+  Modal,
+  Button,
+} from "react-native";
+import { Card, Icon, Input } from "@rneui/themed";
 import { baseUrl } from "../comun/comun";
 import { connect } from "react-redux";
 import { favoritos } from "../redux/favoritos";
-import { postFavorito } from "../redux/ActionCreators";
+import { postFavorito, postComentario } from "../redux/ActionCreators";
+import { colorGaztaroaOscuro } from "../comun/comun";
+import { Rating } from "react-native-ratings";
 
 const mapStateToProps = (state) => {
   return {
@@ -16,34 +26,118 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = (dispatch) => ({
   postFavorito: (excursionId) => dispatch(postFavorito(excursionId)),
+  postComentario: (comentario) => dispatch(postComentario(comentario)),
 });
 
 function RenderExcursion(props) {
   const excursion = props.excursion;
+  const [modalVisible, setModalVisible] = useState(false);
+  const [rating, setRating] = useState(5);
+  const [author, setAuthor] = useState("");
+  const [comment, setComment] = useState("");
+
+  const resetModal = () => {
+    setModalVisible(false);
+    setRating(5);
+    setAuthor("");
+    setComment("");
+  };
+
+  const gestionarComentario = () => {
+    const newComment = {
+      excursionId: props.excursion.id,
+      valoracion: rating,
+      autor: author,
+      comentario: comment,
+      dia: new Date().toISOString(),
+    };
+    props.onSubmitComment(newComment);
+    resetModal();
+  };
 
   if (excursion != null) {
     const imageUrl = baseUrl + excursion.imagen;
     return (
-      <Card>
-        <View style={styles.cardContainer}>
-          <Text style={styles.title}>{excursion.nombre}</Text>
-          <View style={styles.dividerContainer}></View>
-          <Card.Image source={{ uri: imageUrl }} />
-        </View>
-        <Text style={styles.description}>{excursion.descripcion}</Text>
-        <Icon
-          raised
-          reverse
-          name={props.favorita ? "heart" : "heart-o"}
-          type="font-awesome"
-          color="#f50"
-          onPress={() =>
-            props.favorita
-              ? console.log("La excursión ya se encuentra entre las favoritas")
-              : props.onPress()
-          }
-        />
-      </Card>
+      <>
+        <Card>
+          <View style={styles.cardContainer}>
+            <Text style={styles.title}>{excursion.nombre}</Text>
+            <View style={styles.dividerContainer}></View>
+            <Card.Image source={{ uri: imageUrl }} />
+          </View>
+          <Text style={styles.description}>{excursion.descripcion}</Text>
+          <View style={styles.iconContainer}>
+            <Icon
+              raised
+              reverse
+              name={props.favorita ? "heart" : "heart-o"}
+              type="font-awesome"
+              color="#f50"
+              onPress={() => (props.favorita ? null : props.onPress())}
+            />
+            <Icon
+              raised
+              reverse
+              name="pencil"
+              type="font-awesome"
+              color={colorGaztaroaOscuro}
+              onPress={() => setModalVisible(true)}
+            />
+          </View>
+        </Card>
+        <Modal
+          animationType="slide"
+          transparent={false}
+          visible={modalVisible}
+          onRequestClose={resetModal}
+        >
+          <View style={styles.modalContainer}>
+            <View style={styles.ratingContainer}>
+              <Text style={styles.ratingText}>
+                Rating: <Text style={styles.ratingNumber}>{rating}</Text>/5
+              </Text>
+              <Rating
+                type="star"
+                startingValue={rating}
+                imageSize={30}
+                fractions={0}
+                onFinishRating={(value) => setRating(value)}
+              />
+            </View>
+            <Input
+              placeholder="Autor"
+              leftIcon={{
+                type: "font-awesome",
+                name: "user",
+                containerStyle: styles.iconSpacing,
+              }}
+              value={author}
+              onChangeText={(text) => setAuthor(text)}
+              containerStyle={styles.inputContainer}
+            />
+            <Input
+              placeholder="Comentario"
+              leftIcon={{
+                type: "font-awesome",
+                name: "comment",
+                containerStyle: styles.iconSpacing,
+              }}
+              value={comment}
+              onChangeText={(text) => setComment(text)}
+              multiline
+              containerStyle={styles.inputContainer}
+            />
+            <View style={styles.buttonContainer}>
+              <Button
+                title="ENVIAR"
+                onPress={gestionarComentario}
+                color="#0000FF"
+              />
+              <Button title="CANCELAR" onPress={resetModal} color="#0000FF" />
+            </View>
+          </View>
+        </Modal>
+      </>
     );
   } else {
     return <View></View>;
@@ -78,6 +172,33 @@ function RenderComentario(props) {
 }
 
 class DetalleExcursion extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      valoracion: 5,
+      autor: "",
+      comentario: "",
+      showModal: false,
+    };
+  }
+
+  toggleModal() {
+    this.setState({ showModal: !this.state.showModal });
+  }
+
+  resetForm() {
+    this.setState({
+      valoracion: 3,
+      autor: "",
+      comentario: "",
+      showModal: false,
+    });
+  }
+
+  gestionarComentario(nuevoComentario) {
+    this.props.postComentario(nuevoComentario);
+  }
+
   marcarFavorito(excursionId) {
     this.props.postFavorito(excursionId);
   }
@@ -86,17 +207,20 @@ class DetalleExcursion extends Component {
     const { excursionId } = this.props.route.params;
     const excursion = this.props.excursiones.excursiones[+excursionId];
     const comentarios = this.props.comentarios.comentarios.filter(
-      (comentario) => comentario.excursionId === excursionId
+      (comentario) => comentario.excursionId === +excursionId
     );
 
     return (
       <ScrollView>
         <RenderExcursion
-          excursion={this.props.excursiones.excursiones[+excursionId]}
+          excursion={excursion}
           favorita={this.props.favoritos.favoritos.some(
             (el) => el === excursionId
           )}
           onPress={() => this.marcarFavorito(excursionId)}
+          onSubmitComment={(nuevoComentario) =>
+            this.gestionarComentario(nuevoComentario)
+          }
         />
         <RenderComentario comentarios={comentarios} />
       </ScrollView>
@@ -115,6 +239,46 @@ const styles = StyleSheet.create({
   },
   description: {
     margin: 20,
+  },
+  iconContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    marginVertical: 10,
+  },
+  modalContainer: {
+    flex: 1,
+    alignItems: "center",
+    margin: 20,
+    justifyContent: "flex-start",
+    marginTop: 80,
+  },
+  ratingContainer: {
+    alignItems: "center",
+    marginBottom: 20,
+  },
+  ratingText: {
+    fontSize: 24,
+    color: "#FFD700",
+    marginBottom: 10,
+  },
+  ratingNumber: {
+    fontSize: 34,
+    fontWeight: "bold",
+    color: "#FFD700",
+  },
+  iconSpacing: {
+    marginRight: 10,
+  },
+  inputContainer: {
+    marginBottom: 20,
+    width: "80%",
+  },
+  buttonContainer: {
+    alignItems: "center",
+    width: "80%",
+  },
+  buttonSpacing: {
+    height: 10,
   },
 });
 
--- appGaztaroaJavascript/redux/comentarios.js
+++ appGaztaroaJavascript/redux/comentarios.js
@@ -1,12 +1,24 @@
-import * as ActionTypes from './ActionTypes';
+import * as ActionTypes from "./ActionTypes";
 
-export const comentarios = (state = { errMess: null, comentarios:[]}, action) => {
+export const comentarios = (
+  state = { errMess: null, comentarios: [] },
+  action
+) => {
   switch (action.type) {
     case ActionTypes.ADD_COMENTARIOS:
-      return {...state, errMess: null, comentarios: action.payload};
+      return { ...state, errMess: null, comentarios: action.payload };
 
     case ActionTypes.COMENTARIOS_FAILED:
-      return {...state, errMess: action.payload};
+      return { ...state, errMess: action.payload };
+
+    case ActionTypes.ADD_COMENTARIO:
+      const newComment = {
+        ...action.payload,
+      };
+      return {
+        ...state,
+        comentarios: [...state.comentarios, newComment],
+      };
 
     default:
       return state;
